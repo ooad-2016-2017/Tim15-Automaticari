e9df9079bfc6be5737722571b75d38b69691e514
Mirza Hodovic <mhodovic2@etf.unsa.ba>
2017-05-28 15:24:19 +0200

    Pokusaj baze (neuspjeli)

---
+++ Projekat - BrzaNaplataPutarine/BrzaNaplataPutarine/BrzaNaplataPutarine/Models/Uredjaj.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrzaNaplataPutarine.Models
+{
+    class Uredjaj
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int idUredjaja;
+        private double stanje;
+        
+        public Uredjaj(int idUredjaja, double pocetnoStanje)
+        {
+            IdUredjaja = idUredjaja;
+            Stanje = pocetnoStanje;
+        }
+
+        public int IdUredjaja
+        {
+            get
+            {
+                return idUredjaja;
+            }
+
+            set
+            {
+                idUredjaja = value;
+            }
+        }
+
+        public double Stanje
+        {
+            get
+            {
+                return stanje;
+            }
+
+            set
+            {
+                stanje = value;
+            }
+        }
+    }
+}
