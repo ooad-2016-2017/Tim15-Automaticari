51536be1b2cc8ef0b9cb3120a8c590f8723f73eb
Danijel Cuturic <dcuturic1@etf.unsa.ba>
2017-06-08 20:16:52 +0200

    dodan Model - klasa naplata (zaboravljena ranije)

---
+++ BrzaNaplataPutarineApp/BrzaNaplataPutarineApp/Model/Naplata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrzaNaplataPutarineApp.Model
+{
+    class Naplata
+    {
+        private int broj_naplate;
+        //private Vrijednost vrijednost;
+
+        public int Broj_naplate
+        {
+            get
+            {
+                return broj_naplate;
+            }
+
+            set
+            {
+                broj_naplate = value;
+            }
+        }
+    }
+}
